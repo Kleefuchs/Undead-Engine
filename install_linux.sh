154bff6f9a9c5814f2f2e401a5cd151f580bd5a2
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2024-04-30 17:11:54 +0200

    Added Installation scripts for Linux

---
+++ install_linux.sh
@@ -0,0 +1,4 @@
+#include location for gcc: /usr/local/include
+#lib location for gcc: /usr/local/lib
+cd target
+sudo cp -r include /usr/local/include/UndeadEngine
---
+++ uninstall_linux.sh
@@ -0,0 +1,4 @@
+#include location for gcc: /usr/local/include
+#lib location for gcc: /usr/local/lib
+
+sudo rm -r /usr/local/include/UndeadEngine
